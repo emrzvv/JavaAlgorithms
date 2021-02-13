3e5da5f9de3e404dcd589ce4e091b11459f5c2c3
emrzvv <e.mrzvv@gmail.com>
2021-02-13 15:47:15 +0300

    added tests for Queue class

---
+++ queue/src/main/QueueTest.java
@@ -0,0 +1,19 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class QueueTest {
+
+    @Test
+    void enqueue() {
+    }
+
+    @Test
+    void dequeue() {
+    }
+
+    @Test
+    void size() {
+    }
+}
