c4a79531f839be61d7374803d2483e893a71a8ec
emrzvv <e.mrzvv@gmail.com>
2021-02-21 15:48:49 +0300

    added HashTable class

---
+++ hash_table/src/main/HashTable.java
@@ -0,0 +1,106 @@
+package main;
+
+public class HashTable
+{
+    private final int [] powers;
+
+    public int size;
+    public int step;
+    public String [] slots;
+    public int BASE = 2017;
+    public int MOD;
+
+    public HashTable(int _size, int _step)
+    {
+        size = _size;
+        step = _step;
+        MOD = size;
+        slots = new String[size];
+        powers = new int[size];
+        powers[0] = 1;
+        for (int i = 0; i < size; ++i) {
+            if (i > 0) {
+                powers[i] = (powers[i - 1] * BASE + MOD) % MOD;
+            }
+            slots[i] = null;
+        }
+        for (int i = 0; i < 40; ++i) {
+            System.out.print(powers[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+    public int hashFun(String value)
+    {
+        int n = value.length();
+        int hash = 0;
+        for (int i = 0; i < n; ++i) {
+            hash += powers[i] * (value.charAt(i) - 'a' + 1);
+            hash += MOD;
+            hash %= MOD;
+        }
+        return hash;
+    }
+
+    public int seekSlot(String value)
+    {
+        int hash = hashFun(value);
+        int firstHash = hash;
+        boolean passed = false;
+        while (slots[hash] != null) {
+            hash += step;
+            if (hash >= size) passed = true;
+            hash %= MOD;
+            if (passed && hash >= firstHash) {
+                break;
+            }
+        }
+        if (slots[hash] != null || passed && hash >= firstHash) {
+            return -1;
+        }
+        else {
+            return hash;
+        }
+    }
+
+    public int put(String value)
+    {
+        int idx = seekSlot(value);
+        if (idx != -1) {
+            slots[idx] = value;
+        }
+        return idx;
+    }
+
+    public int find(String value)
+    {
+        int hash = hashFun(value);
+        int firstHash = hash;
+        boolean passed = false;
+        while (slots[hash] != null && slots[hash].compareTo(value) != 0) {
+            hash += step;
+            if (hash >= size) {
+                passed = true;
+            }
+            hash %= MOD;
+            if (passed && hash >= firstHash) {
+                break;
+            }
+        }
+        if (slots[hash] == null || slots[hash].compareTo(value) != 0) {
+            return -1;
+        }
+        else {
+            return hash;
+        }
+    }
+
+    public void outputNonNull() {
+        for (int i = 0; i < size; ++i) {
+            if (slots[i] != null ){
+                System.out.println(i + " - " + slots[i]);
+            }
+        }
+    }
+}
---
+++ hash_table/src/main/Main.java
@@ -0,0 +1,34 @@
+package main;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    HashTable table = new HashTable(2011, 1);
+	    Scanner s = new Scanner(System.in);
+        int req = s.nextInt();
+        for (int q = 0; q < req; ++q) {
+            int type = s.nextInt();
+            if (type == 1) {
+                String line = s.nextLine();
+                if (table.put(line) != -1) {
+                    System.out.println("inserted " + line);
+                }
+                else {
+                    System.out.println("not inserted");
+                }
+            }
+            else if (type == 2) {
+                String line = s.nextLine();
+                int result = table.find(line);
+                if (result != -1) {
+                    System.out.println(result);
+                }
+                else {
+                    System.out.println("not found");
+                }
+            }
+            table.outputNonNull();
+        }
+    }
+}
