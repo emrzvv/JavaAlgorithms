7e4aabd0253ec31507f789b28eb72ea7f1165370
emrzvv <e.mrzvv@gmail.com>
2021-03-03 13:29:36 +0300

    added associative array

---
+++ associative_array/src/main/Main.java
@@ -0,0 +1,38 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+
+        NativeDictionary<Integer> map = new NativeDictionary<>(100, Integer.class);
+        int req = s.nextInt();
+        for (int q = 0; q < req; ++q) {
+            int type = s.nextInt();
+            if (type == 1) {
+                String key = s.next();
+                int value = s.nextInt();
+                map.put(key, value);
+            }
+            else if (type == 2) {
+                String key = s.next();
+                if (map.isKey(key)) {
+                    System.out.println("YES");
+                }
+                else {
+                    System.out.println("NO");
+                }
+            }
+            else if (type == 3) {
+                String key = s.next();
+                System.out.println(map.get(key));
+            }
+            else if (type == 4) {
+                map.outputNonNull();
+            }
+        }
+    }
+}
---
+++ associative_array/src/main/NativeDictionary.java
@@ -0,0 +1,118 @@
+package main;
+
+import java.lang.reflect.Array;
+
+class NativeDictionary<T>
+{
+    public int size;
+    public String [] slots;
+    public T [] values;
+    private int MOD;
+    private int BASE = 2017;
+    private int step = 3;
+    private final int [] powers;
+
+
+    public NativeDictionary(int sz, Class clazz)
+    {
+        size = sz;
+        slots = new String[size];
+        values = (T[]) Array.newInstance(clazz, this.size);
+        MOD = size;
+        powers = new int[size];
+        slots[0] = null;
+        values[0] = null;
+        for (int i = 1; i < size; ++i) {
+            powers[i] = (powers[i - 1] * BASE + MOD) % MOD;
+            slots[i] = null;
+            values[i] = null;
+        }
+    }
+
+    public int seekSlot(String value, int hash) {
+        int firstHash = hash;
+        boolean passed = false;
+        while (slots[hash] != null) {
+            hash += step;
+            if (hash >= size) passed = true;
+            hash %= MOD;
+            if (passed && hash >= firstHash) {
+                break;
+            }
+        }
+        if (slots[hash] != null || passed && hash >= firstHash) {
+            return -1;
+        }
+        else {
+            return hash;
+        }
+    }
+
+    public int hashFun(String key)
+    {
+        int n = key.length();
+        int hash = 0;
+        for (int i = 0; i < n; ++i) {
+            hash += powers[i] * ((int)key.charAt(i) + 1);
+            hash += MOD;
+            hash %= MOD;
+        }
+        return hash;
+    }
+
+    private int getHelper(String key) {
+        int idx = hashFun(key);
+        int firstIdx = idx;
+        boolean passed = false;
+        while (true) {
+            if (slots[idx] != null) {
+                if (slots[idx].equals(key)) {
+                    break;
+                }
+            }
+            idx += step;
+            if (idx >= size) {
+                passed = true;
+            }
+            idx %= MOD;
+            if (passed && idx >= firstIdx) {
+                break;
+            }
+        }
+        if (slots[idx] == null || !slots[idx].equals(key) || passed && idx >= firstIdx) {
+            return -1;
+        }
+        return idx;
+    }
+
+    public boolean isKey(String key)
+    {
+        return getHelper(key) != -1;
+    }
+
+    public void put(String key, T value)
+    {
+        int idx = seekSlot(key, hashFun(key));
+        slots[idx] = key;
+        values[idx] = value;
+    }
+
+    public T get(String key)
+    {
+        int idx = getHelper(key);
+        if (idx != -1) {
+            return values[idx];
+        }
+        else {
+            return null;
+        }
+    }
+
+    public void outputNonNull() {
+        for (int i = 0; i < size; ++i) {
+            if (slots[i] != null) {
+                System.out.println(slots[i] + " : " + values[i]);
+            }
+        }
+    }
+}
